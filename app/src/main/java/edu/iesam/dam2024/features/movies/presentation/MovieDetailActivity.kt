c565f6804a173554b92b1790eb2145f5d71bfa3c
heennaarr <henar.herher@educa.jcyl.es>
2024-10-07 20:21:32 +0200

    Añadido observador de detail de movie

--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import edu.iesam.dam2024.app.extensions.loadUrl
 import com.bumptech.glide.Glide
 import edu.iesam.dam2024.R
@@ -20,10 +21,29 @@ class MovieDetailActivity : AppCompatActivity() {
         movieFactory = MovieFactory(this)
         viewModel = movieFactory.buildMovieDetailViewModel()
         getMovieId()?.let { movieId ->
-            viewModel.viewCreated(movieId)?.let { movie ->
-                bindData(movie)
+            viewModel.viewCreated(movieId)
+        }
+        setupObserver()
+    }
+    private fun setupObserver() {
+        val movieObserver = Observer<MovieDetailViewModel.UiState> { uiState ->
+            uiState.movie?.let {
+                bindData(it)
+            }
+            uiState.errorApp?.let {
+                //pinto el error
+            }
+            if (uiState.isLoading) {
+                //muestro el cargando...
+                Log.d("@dev", "Cargando...")
+            } else {
+                //oculto el cargando...
+                Log.d("@dev"," Cargado ...")
             }
         }
+        viewModel.uiState.observe(this, movieObserver)
+
+
     }
     private fun getMovieId(): String? {
         return intent.getStringExtra(KEY_MOVIE_ID)
--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailViewModel.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieDetailViewModel.kt
@@ -1,13 +1,30 @@
 package edu.iesam.dam2024.features.movies.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.iesam.dam2024.features.movies.domain.ErrorApp
 import edu.iesam.dam2024.features.movies.domain.GetMovieSelectedUseCase
 import edu.iesam.dam2024.features.movies.domain.GetMoviesUseCase
 import edu.iesam.dam2024.features.movies.domain.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 class MovieDetailViewModel(
     private val getMovieSelectedUseCase: GetMovieSelectedUseCase
 ) : ViewModel() {
-    fun viewCreated(movieId: String): Movie? {
-        return getMovieSelectedUseCase.invoke(movieId)
+    private val _uiState = MutableLiveData<UiState>()
+    val uiState : LiveData<UiState> = _uiState
+    fun viewCreated(movieId: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            val movie = getMovieSelectedUseCase.invoke(movieId)
+            _uiState.postValue(UiState(movie = movie))
+        }
     }
+    data class UiState(
+        val isLoading: Boolean = false,
+        val errorApp: ErrorApp? = null,
+        val movie : Movie? = null
+    )
 }
